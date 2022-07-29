1da464f1d9e476acc02114cb3cb8aca00d99b293
tektechnologies <info@tektechnologies.com>
2022-07-28 22:27:38 -0500

    add file set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import './App.css';
+import React from 'react';
+import Header from './Header.js';
+import Main from './Main.js';
+import Footer from './Footer.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+     
+    )
+  }
 }
 
 export default App;
